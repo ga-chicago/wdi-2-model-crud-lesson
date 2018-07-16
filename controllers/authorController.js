ce10630bb1d78dc08d9a2a61114f041c8151c143
Reuben Ayres <ra@sent.com>
2018-07-16 11:57:37 -0500

    Author is being added with mongoose in author create route

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Author = require('../models/author')
 
 // author index route
 router.get('/', (req, res) => {
@@ -15,8 +16,18 @@ router.get('/new', (req, res) => {
 router.post('/', (req, res) => {
   // see what user typed
   console.log(req.body)
-  // just to see if post route set up right
-  res.send('you posted, check terminal to see req.body');
+
+  // Use mongoose to add the author user entered
+  Author.create(req.body, (err, createdAuthor) => {
+    if(err) console.log('mongoose query error in author create route', err);
+    else {
+      console.log(createdAuthor)
+      console.log("^^^^here's the author you created")
+      res.redirect('/authors')
+    }
+    
+  })
+
 })
 
 module.exports = router;
