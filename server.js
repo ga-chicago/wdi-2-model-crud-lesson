79be3d572c43db3d55f0146b61f743257a1f6930
Reuben Ayres <ra@sent.com>
2018-07-16 11:06:15 -0500

    added mongoose and used it to connect to db from our express app

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// run our database connection code
+require('./db/db');
+
 app.listen(3000, () => {
   console.log("Express server running")
 })
