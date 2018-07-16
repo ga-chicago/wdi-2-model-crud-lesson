94d0545f01791722405fb2d0cca099b0d76b64eb
Reuben Ayres <ra@sent.com>
2018-07-16 11:16:29 -0500

    rendering a template from app.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 // run our database connection code
 require('./db/db');
 
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+
 app.listen(3000, () => {
   console.log("Express server running")
 })
